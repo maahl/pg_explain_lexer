346e8a5d0da23d2a76dfcbd54dca6216a07a76ba
Maxence Ahlouche <maxence.ahlouche@gmail.com>
2018-10-28 16:25:38 +0100

    Handle explain header

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 test:
-	cd test && xelatex -shell-escape test.tex
+	cd test && xelatex -shell-escape test.tex && rm -r _minted-test
 
 .PHONY: test
--- pg_explain_lexer.py
+++ pg_explain_lexer.py
@@ -22,10 +22,16 @@ class PgExplainLexer(RegexLexer):
             (r'(Index Scan using )(\w)( on )', bygroups(Keyword.Type, 'object_name', Keyword.Type), 'object_name'),
             (r'(Sort Method|Join Filter|Rows Removed by Join Filter|Rows Removed by Filter|Planning Time|Execution Time)', Comment.Preproc),
             (r'(Sort|Nested Loop|Seq Scan on|Merge Join|Index Scan using)', Keyword.Type),
+            # strings
             (r"'(''|[^'])*'", String.Single),
+            # numbers
             (r'[0-9]+(\.[0-9]+)?', Name.Literal),
+            # explain header
+            (r'\s*QUERY PLAN', Comment.Single),
+            (r'-+', Comment.Single),
         ],
         'object_name': [
+            # matches possibly schema-qualified table and column names
             (r'\w+(\.\w+)*', Name.Variable.Instance),
         ],
     }
