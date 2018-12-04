ebc687767bbcf9a7bfe6610df3f6e13cd56652f1
Maxence Ahlouche <maxence.ahlouche@gmail.com>
2018-12-05 00:34:46 +0900

    Support merge append node and add a state for parenthesized expressions

--- pg_explain_lexer.py
+++ pg_explain_lexer.py
@@ -20,22 +20,32 @@ class PgExplainLexer(RegexLexer):
             # operator arguments or details
             (r'(Sort Method|Join Filter|Rows Removed by Join Filter|Rows Removed by Filter|Planning Time|Execution Time|Heap Fetches|Heap Blocks|Workers (Planned|Launched)|never executed)', Comment.Preproc),
             # simple keywords
-            (r'(Sort|Nested Loop Left Join|Nested Loop|Merge Join|Hash Right Join|(Parallel )?Hash Join|(Parallel )?Hash|Limit|(Finalize |Partial )?Aggregate|Materialize|Gather( Merge )?)', Keyword.Type),
+            (r'(Sort|Nested Loop Left Join|Nested Loop|Merge Join|Hash Right Join|(Parallel )?Hash Join|(Parallel )?Hash|Limit|(Finalize |Partial )?Aggregate|Materialize|Gather( Merge )?)|Merge Append', Keyword.Type),
             # strings
             (r"'(''|[^'])*'", String.Single),
             # numbers
             (r'[0-9]+(\.[0-9]+)?', Name.Literal),
             # explain header
-            (r'\s*QUERY PLAN', Comment.Single),
-            (r'-+', Comment.Single),
+            (r'\s*QUERY PLAN\s*\n-+', Comment.Single),
+        ],
+        'expression': [
+            # matches any kind of parenthesized expression
+            # the first opening paren is matched by the 'caller'
+            (r'\(', Punctuation, 'expression'),
+            (r'\)', Punctuation, '#pop'),
+            (r'[^)]*', Name.Variable),
         ],
         'object_name': [
             # matches possibly schema-qualified table and column names
-            (r'\w+(\.\w+)*( \w+)?', Name.Variable),
+            # if object_name is parenthesized, mark opening paren as
+            # punctuation, call 'expression', and exit state
+            (r'\(', Punctuation, 'expression'),
+            (r'\)', Punctuation, '#pop'),
+            (r'\w+(\.\w+)*( USING \S+| \w+ USING \S+| \w)?', Name.Variable),
             (r'', Punctuation, '#pop'),
         ],
         'predicate': [
-            # if predicate is parenthesized, mark parens as punctuation
+            # if predicate is parenthesized, mark paren as punctuation
             (r'(\()([^\n]*)(\))', bygroups(Punctuation, Name.Variable, Punctuation), '#pop'),
             # otherwise color until newline
             (r'[^\n]*', Name.Variable, '#pop'),
