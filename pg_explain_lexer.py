606f9ed51c4dee467f2f687f71fa5063db7ccb09
Maxence Ahlouche <maxence.ahlouche@gmail.com>
2019-02-07 12:04:10 +0100

    Handle Insert, Result and ProjectSet nodes

--- pg_explain_lexer.py
+++ pg_explain_lexer.py
@@ -14,13 +14,13 @@ class PgExplainLexer(RegexLexer):
             (r'(Sort Key|Group Key)(: )', bygroups(Comment.Preproc, Punctuation), 'object_name'),
             (r'(Sort Method)(: )', bygroups(Comment.Preproc, Punctuation), 'object_name'),
             (r'(Join Filter|Filter|Merge Cond|Hash Cond|Index Cond|Recheck Cond)(: )', bygroups(Comment.Preproc, Punctuation), 'predicate'),
-            (r'(Parallel )?Seq Scan on |(Parallel )?Bitmap Heap Scan on |Bitmap Index Scan on |Subquery Scan on ', Keyword.Type, 'object_name'),
+            (r'(Parallel )?Seq Scan on |(Parallel )?Bitmap Heap Scan on |Bitmap Index Scan on |Subquery Scan on |Insert on ', Keyword.Type, 'object_name'),
             # "using" operators
             (r'((?:Parallel )?Index (?:Only )?Scan using )(\w+(?:\.\w+)*)( on )', bygroups(Keyword.Type, Name.Variable, Keyword.Type), 'object_name'),
             # operator arguments or details
             (r'(Sort Method|Join Filter|Rows Removed by Join Filter|Rows Removed by Filter|Planning Time|Execution Time|Heap Fetches|Heap Blocks|Workers (Planned|Launched)|never executed)', Comment.Preproc),
             # simple keywords
-            (r'(Sort|Nested Loop Left Join|Nested Loop|Merge Join|Hash (Right|Left|Full) Join|(Parallel )?Hash Join|(Parallel )?Hash|Limit|(Finalize |Partial )?Aggregate|Materialize|Gather( Merge)?)|(Merge )?Append', Keyword.Type),
+            (r'(Sort|Nested Loop Left Join|Nested Loop|Merge Join|Hash (Right|Left|Full) Join|(Parallel )?Hash Join|(Parallel )?Hash|Limit|(Finalize |Partial )?Aggregate|Materialize|Gather( Merge)?)|(Merge )?Append|Result|ProjectSet', Keyword.Type),
             # strings
             (r"'(''|[^'])*'", String.Single),
             # numbers
@@ -44,6 +44,8 @@ class PgExplainLexer(RegexLexer):
             (r'\w+(\.\w+)*( USING \S+| \w+ USING \S+| \w)?', Name.Variable),
             # if we encounter a comma, another object is listed
             (r', ', Punctuation, 'object_name'),
+            # special case: "*SELECT*"
+            (r'"\*SELECT\*"', Name.Variable),
             (r'', Punctuation, '#pop'),
         ],
         'predicate': [
